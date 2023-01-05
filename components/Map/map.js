7eeca1afb661ebc1668b2011b4c122bd14be9c02
daesanTheRiceologist <dkim265@my.bcit.ca>
2023-01-05 11:18:27 -0800

    next app created

---
+++ components/Map/map.js
@@ -0,0 +1,5 @@
+import 'leaflet/dist/leaflet.css'
+import 'leaflet-defaulticon-compatibility'
+import 'leaflet-defaulticon-compatibility/dist/leaftlet-defaulticon-compatibility.css'
+
+import { MapContainer, TileLayer, Marker, Popup, Polyline, CircleMaker } from 'react-leaflet'
